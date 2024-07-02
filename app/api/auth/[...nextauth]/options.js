227c25abb49c24f7d54158f698393bf4a7286e0d
Sumit <sumit8444061@gmail.com>
2024-07-02 10:35:52 +0500

    commit

--- app/api/auth/[...nextauth]/options.js
+++ app/api/auth/[...nextauth]/options.js
@@ -1,4 +1,3 @@
-"use server"
 import GoogleProvider from "next-auth/providers/google"
 import GithubProvider from "next-auth/providers/github"
 import CredentialsProvider from "next-auth/providers/credentials"
--- app/api/users/route.js
+++ app/api/users/route.js
@@ -1,31 +1,32 @@
-"use server"
-import { createUserWithAccount,getUserByEmail } from "@/utils/user";
+import { createUserWithAccount, getUserByEmail } from "@/utils/user";
 import { NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
 
-
-export const POST = async (req, res) => {
+export const POST = async (req) => {
     try {
-        const { name, email, password } = await req.json()
-        const existingUser = await getUserByEmail(email)
+        const { name, email, password } = await req.json();
+        const existingUser = await getUserByEmail(email);
+        
         if (existingUser) {
-          return NextResponse.json({message : "User already exists"}, { status: 400 });
+            return NextResponse.json({ message: "User already exists" }, { status: 400 });
         }
+
         const hashedPassword = await bcrypt.hash(password, 10);
-        const newUser= await createUserWithAccount({
+        const newUser = await createUserWithAccount({
             name,
             email,
             password: hashedPassword,
-            });
+        });
 
-        return NextResponse.json({message : "User created successfully",
-             data:{...newUser}}
-             , { status: 201 });
-        
+        return NextResponse.json({
+            message: "User created successfully",
+            data: { ...newUser }
+        }, { status: 201 });
 
     } catch (error) {
-    
-      return NextResponse.json({message : "Error" , error:error.message}, { status: 500 });
- 
+        return NextResponse.json({
+            message: "Error",
+            error: error.message
+        }, { status: 500 });
     }
-}
+};
